c080e08cdc55b12d61ea32001a7344b22b4b66e2
FaustineBrachotte <105442482+FaustineBrachotte@users.noreply.github.com>
2024-11-07 15:22:04 +0100

    update cross behavior

--- game.js
+++ game.js
@@ -8,15 +8,29 @@ function drawCheck() {
 	check.classList.add('animate');
 }
 
-function isDead() {
-	hideBtns();
-	const container = document.getElementById('isDead');
-	container.style.visibility = 'visible';
-	if (container.textContent == 'Vivant') {
-		drawCross();
-	} else if (container.textContent == 'Mort') {
-		drawCheck();
+function playDead() {
+	showDeath();
+	const isDead = document.getElementById('isDead');
+	isDead.style.visibility = 'visible';
+	if (isDead.textContent == 'Vivant') {
+		isDead.textContent = 'Faux, il est vivant !';
+	} else if (isDead.textContent == 'Mort') {
+		isDead.textContent = 'En effet, il est mort.';
+		markAsDead();
 	}
+}
+
+function markAsDead() {
+	const circle = document.getElementById('circle');
+	circle.style.transition = 'background-color 0.5s ease, color 0.5s ease';
+	circle.style.backgroundColor = '#350707';
+	circle.style.color = 'white';
+	drawCross();
+}
+
+function showDeath() {
+	const isDead = document.getElementById('isDead');
+	isDead.style.visibility = 'visible';
 }
 
 function hideBtns() {
@@ -35,17 +49,47 @@ function showBtns() {
 
 function isAlive() {
 	hideBtns();
-	const container = document.getElementById('isDead');
-	container.style.visibility = 'visible';
-	if (container.textContent == 'Mort') {
+	const isDead = document.getElementById('isDead');
+	const sexe = document.getElementById('sexe');
+
+	isDead.style.visibility = 'visible';
+	if (isDead.textContent == 'Mort' && sexe.textContent == 'Male') {
+		isDead.textContent = 'Et non, il est mort !';
 		drawCross();
-	} else if (container.textContent == 'Vivant') {
-		drawCheck();
+	} else if (isDead.textContent == 'Vivant' && sexe.textContent == 'Female') {
+		isDead.textContent = 'Et non, elle est morte !';
+		drawCross();
+	} else if (isDead.textContent == 'Vivant' && sexe.textContent == 'Male') {
+		isDead.textContent = 'Bravo, il est vivant !';
+	} else if (isDead.textContent == 'Vivant' && sexe.textContent == 'Female') {
+		isDead.textContent = 'Bravo, elle est vivante !';
 	}
 }
 
 async function fetchCharacter() {
+	resumePlayState();
+	const randomNumber = Math.floor(Math.random() * 2100) + 1;
+	try {
+		const response = await fetch(
+			`https://www.anapioficeandfire.com/api/characters/${randomNumber}`
+		);
+
+		const data = await response.json();
+		displayCharacter(data);
+		displayDeath(data);
+		displaySexe(data);
+	} catch (error) {
+		console.error(error);
+	}
+	showBtns();
+}
+
+function resumePlayState() {
 	const cross = document.querySelector('.cross');
+	const circle = document.getElementById('circle');
+	circle.style.backgroundColor = '#7dbdf1';
+	circle.style.color = 'black';
+
 	if (cross !== null) {
 		cross.classList.remove('animate');
 	}
@@ -54,27 +98,17 @@ async function fetchCharacter() {
 	if (check !== null) {
 		check.classList.remove('animate');
 	}
-
-	const randomNumber = Math.floor(Math.random() * 2100) + 1;
-
-	try {
-		const response = await fetch(
-			`https://www.anapioficeandfire.com/api/characters/${randomNumber}`
-		);
-
-		const data = await response.json();
-		console.log(data);
-		displayCharacter(data);
-		displayDeath(data);
-	} catch (error) {
-		console.error(error);
-	}
-	showBtns();
 }
 
 function displayCharacter(data) {
 	const div = document.getElementById('characterName');
 	div.textContent = data.name;
+}
+
+function displaySexe(data) {
+	const div = document.getElementById('sexe');
+	div.textContent = data.gender;
+	div.style.visibility = 'hidden';
 }
 
 function displayDeath(data) {
